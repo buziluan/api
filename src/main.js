ac50cea8e0c5537c300d6106e4e68858ff81aeaa
fangxu <354952530@qq.com>
2019-03-19 12:37:49 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import iView from 'iview'
+import axios from 'axios'
+import 'iview/dist/styles/iview.css'
+Vue.use(iView)
+var instance = axios.create()
+Vue.prototype.$http = instance
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        {{name}}
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                url: "https://www.easy-mock.com/mock/5c07450556b02a1aed875082/example"
+            }
+        },
+        computed: {
+            name() {
+                return this.$store.state.name
+            }
+        },
+        created() {
+            this.test()
+            this.test1()
+        },
+        methods: {
+            async test() {
+                let res = await this.$http.get(this.url + '/gettest1');
+                console.log(res)
+            },
+            async test1() {
+                let res = await this.$http.get(this.url + '/gettest');
+                console.log(res)
+            }
+        }
+    }
+</script>
